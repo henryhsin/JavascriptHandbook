06a9c4a9fcc156bd0d356bf9c777a5de66977d08
HenryHsin <hooy123456@gmail.com>
2017-05-02 16:49:18 +0800

    install Babel and gulp-babel, add gulpfile.js, add .babelrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 gulp.task('default', function(){
-    
+    //node來源
+    gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+    //瀏覽器來源
+    gulp.src("public/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
 });
